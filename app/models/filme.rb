c4be27aeae2a2efb73184ad7803291499d4b3ff6
Miquéias Belarmino <miqueiascostabac@gmail.com>
2021-03-14 19:53:29 -0300

    validação do formulário de criação de filme

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -1,3 +1,19 @@
 class Filme < ApplicationRecord
 
+    validates :nome, presence: true
+    validates :ano, presence: true, numericality: 
+    { 
+        only_integer: true,
+        greater_than: 0,
+        less_than: 6
+    }
+    validates :sinopse, presence: true
+    validates :diretor, presence: true
+    validates :nota, presence: true,numericality: 
+    { 
+        only_integer: true,
+        greater_than: 0,
+        less_than: 6
+    }
+
 end
